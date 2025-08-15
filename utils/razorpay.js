c1c69077b3c210d2d817dfca5717de2a9fad069a
ShivaBoya <shivasiddu80@gmail.com>
2025-08-15 22:35:55 +0530

    complete

--- utils/razorpay.js
+++ utils/razorpay.js
@@ -11,8 +11,8 @@ if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
 }
 
 var instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id:process.env.RAZORPAY_KEY_ID,
+  key_secret:process.env.RAZORPAY_KEY_SECRET,
 });
 
 module.exports = instance;
